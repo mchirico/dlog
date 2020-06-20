26e4bcf03457a2bea7e658b384e51f5eed4db5b6
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-06-20 12:07:17 -0400

    Package install setup

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,123 @@
-import fs from 'fs'
+// import fs from 'fs'
+import { promises, unlinkSync } from 'fs'
 
 class Dlog {
+    silence:boolean
     _file: string
     _statFile: string
     count:number = 0
-    constructor (file:string = './dlogger.txt', statFile:string = './dloggerStats.txt') {
+    constructor (file:string = './dlogger.txt', statFile:string = './dloggerStats.txt', silence = true) {
       this._file = file
       this._statFile = statFile
+      this.silence = silence
     }
 
     set file (file:string) {
       this._file = file
     }
 
-    log (txt: string) {
+    async log (txt: string) {
       const timeStamp = Date()
       this.logStat(timeStamp, `log: {length: ${txt.length}}`)
-      fs.writeFileSync(this._file, `
-        ${timeStamp.toString()}
-          --START--
-        ${txt}
-          --END--
-        `)
-    }
 
-    append (txt: string) {
-      const timeStamp = Date()
-      this.logStat(timeStamp, `append: {length: ${txt.length}}`)
       const data = `
         ${timeStamp.toString()}
           --START--
         ${txt}
           --END--
         `
-      fs.writeFileSync(this._file, data, { flag: 'a' })
+
+      try {
+        await promises.writeFile(this._file, data)
+        console.info('File created successfully with Node.js v13 fs.promises!')
+      } catch (error) {
+        console.error(error)
+      }
+
+      // fs.writeFileSync(this._file, data, { flag: 'w+' })
     }
 
-    read () {
+    async append (txt: string) {
       const timeStamp = Date()
-      const data = fs.readFileSync(this._file)
-      this.logStat(timeStamp, `read: {length: ${data.length}}`)
-      return data.toString()
+      await this.logStat(timeStamp, `append: {length: ${txt.length}}`)
+      const data = `
+        ${timeStamp.toString()}
+          --START--
+        ${txt}
+          --END--
+        `
+      try {
+        await promises.writeFile(this._file, data, { flag: 'a+' })
+        console.info('File created successfully with Node.js v13 fs.promises!')
+      } catch (error) {
+        console.error(error)
+      }
+      // fs.writeFileSync(this._file, data, { flag: 'a+' })
+    }
+
+    async read () {
+      const timeStamp = Date()
+      await this.logStat(timeStamp, 'read')
+      try {
+        const data = await promises.readFile(this._file)
+        return data.toString()
+      } catch (error) {
+        return error
+      }
+    }
+
+    async readStat () {
+      const timeStamp = Date()
+      await this.logStat(timeStamp, 'read')
+      try {
+        const data = await promises.readFile(this._statFile)
+        return data.toString()
+      } catch (error) {
+        return error
+      }
     }
 
     reset () {
-      fs.unlinkSync(this._file)
+      try {
+        unlinkSync(this._file)
+      } catch (error) {
+        if (!this.silence) {
+          console.log(error)
+        }
+      }
     }
 
     resetAll () {
-      fs.unlinkSync(this._file)
-      fs.unlinkSync(this._statFile)
+      try {
+        unlinkSync(this._file)
+      } catch (error) {
+        if (!this.silence) {
+          console.log(error)
+        }
+      }
+
+      try {
+        unlinkSync(this._statFile)
+      } catch (error) {
+        if (!this.silence) {
+          console.log(error)
+        }
+      }
     }
 
-    private logStat (timeStamp: string, txt:string = 'default') {
+    private async logStat (timeStamp: string, txt: string = 'default') {
       const data = `
         ${timeStamp.toString()}: ${this.count}
           ---
                ${txt}
           ---
         `
-      fs.writeFileSync(this._statFile, data, { flag: 'a' })
+      try {
+        await promises.writeFile(this._statFile, data, { flag: 'a+' })
+        console.info('File created successfully with Node.js v13 fs.promises!')
+      } catch (error) {
+        console.error(error)
+      }
     }
 }
 
--- test/index.test.ts
+++ test/index.test.ts
@@ -3,14 +3,24 @@ import { Dlog } from '../src/index'
 describe('Dlog', () => {
   beforeEach(() => {
     const dlog = new Dlog()
-    dlog.reset()
+    dlog.resetAll()
   })
 
   test('creates a simple log', async (done) => {
     const dlog = new Dlog()
     dlog.log('test Message')
-    const results = dlog.read()
+    const results = await dlog.read()
+
     done(expect(results).toContain('test Message'))
+  })
+
+  test('test create stat', async (done) => {
+    const dlog = new Dlog()
+    dlog.resetAll()
+    dlog.log('test Message')
+    const results = await dlog.readStat()
+
+    done(expect(results).toContain('read'))
   })
 
   afterEach(() => {
