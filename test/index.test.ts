f0371f06e1b9fe2554983bf10a16edb5fe643160
Mike Chirico <mchirico@gmail.com>
2020-06-20 14:28:58 -0400

    Promises (#1)
    * npmignore
    
    * Package install setup

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,14 +2,25 @@ import { Dlog } from '../src/index'
 
 describe('Dlog', () => {
   beforeEach(() => {
-
+    const dlog = new Dlog()
+    dlog.resetAll()
   })
 
   test('creates a simple log', async (done) => {
     const dlog = new Dlog()
     dlog.log('test Message')
-    const results = dlog.read()
+    const results = await dlog.read()
+
     done(expect(results).toContain('test Message'))
+  })
+
+  test('test create stat', async (done) => {
+    const dlog = new Dlog()
+    dlog.resetAll()
+    dlog.log('test Message')
+    const results = await dlog.readStat()
+
+    done(expect(results).toContain('read'))
   })
 
   afterEach(() => {
